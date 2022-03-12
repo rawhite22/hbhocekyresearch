ff58f89c7ca48cca12a20874bf66207c49f57d16
robertwhite22 <robertwhite.dev@gmail.com>
2022-03-11 19:40:54 -0500

    stats fix

--- src/pages/player/PlayerStats.js
+++ src/pages/player/PlayerStats.js
@@ -7,15 +7,15 @@ const PlayerStats = ({ stats }) => {
         stats.map((gm) => {
           return (
             <div key={gm.date}>
-              <p>{gm.date}</p>
-              <p>{gm.goals}</p>
-              <p>{gm.assists}</p>
-              <p>{gm.hits}</p>
-              <p>{gm.blocked}</p>
-              <p>{gm.plusMinus}</p>
-              <p>{gm.pim}</p>
-              <p>{gm.powerPlayPoints}</p>
-              <p>{gm.shortHandedPoints}</p>
+              <p>date:{gm.date}</p>
+              <p>goals:{gm.goals}</p>
+              <p>assists:{gm.assists}</p>
+              <p>hits:{gm.hits}</p>
+              <p>blocks:{gm.blocked}</p>
+              <p>+/-{gm.plusMinus}</p>
+              <p>penalty minutes:{gm.pim}</p>
+              <p>ppp:{gm.powerPlayPoints}</p>
+              <p>shp:{gm.shortHandedPoints}</p>
               <p>sht: {gm.shots}</p>
             </div>
           )
